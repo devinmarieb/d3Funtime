6d6d73c8ab0a6b918a322379e5824bd69c0c0a6b
MilkMan90 <mlkaufman747@gmail.com>
2016-12-08 21:03:48 -0700

    BOOM

--- scripts.js
+++ scripts.js
@@ -2,13 +2,9 @@
 var map = new Datamap({
     element: document.getElementById('container'),
     scope: 'usa',
-    fills: {
-      HIGH: '#afafaf',
-      LOW: '#123456',
-      MEDIUM: 'blue',
-      UNKNOWN: 'rgb(0,0,0)',
-      defaultFill: 'green'
-    },
+    // fills: {
+    //   defaultFill: 'green'
+    // },
     geographyConfig: {
       highlightFillColor: '#f1231d',
       popupOnHover: true,
@@ -16,45 +12,75 @@ var map = new Datamap({
         return '<div class="hoverinfo">'+geography.properties+'</div>'
       }
     },
-    data:{
-      NY: {
-        test: 'hello',
-      }
-    }
   });
 
-function data(){
-  return {
-    CA: {
-      fillKey: 'LOW',
-    },
-    NY: {
-      fillKey: 'LOW',
-    },
-    NJ: {
-      fillKey: 'MEDIUM',
-    },
-    CO: {
-      fillKey: 'HIGH',
-    }
-  }
-}
+// function data(){
+//   return {
+//     CA:
+//       fill: "rgb(70, 130, 180)",
+//     },
+//     NY: {
+//       fillKey:
+//           "rgb(159, 47, 51)",
+//     },
+//     NJ: {
+//       fillKey: 'MEDIUM',
+//     },
+//     CO: {
+//       fillKey: 'HIGH',
+//     }
+//   }
+// }
 
-
+const dataSet = "POPESTIMATE2015"
 
 var dataByState = _.keyBy(populationData, "NAME");
 
+dataByState = _.filter(dataByState, function(item){
+  return item.NAME.length < 3
+})
 
-// var color = d3.scale.log()
-//     .range(["hsl(62,100%,90%)", "hsl(228,30%,20%)"])
-//     .interpolate(d3.interpolateHcl);
-//
-// var densities = counties
-//         .map(function(d) { return d.properties.density = d.properties.pop / path.area(d); })
-//         .sort(function(a, b) { return a - b; });
-//
-// color.domain([d3.quantile(densities, .01), d3.quantile(densities, .99)]);
-//
-// console.log(color(20));
+var populationEst2015 = _.map(dataByState, function(state){
+  var stateName=state.NAME;
+  return {
+    state: state.NAME,
+    data: state[dataSet],
+  }
+});
 
-map.updateChoropleth(data());
+var max = _.maxBy(populationEst2015, function(object){
+  return object.data
+});
+
+var min = _.minBy(populationEst2015, function(object){
+  return object.data
+});
+
+console.log(max.data, min.data);
+//
+var color = d3.scaleLinear()
+    .domain([min.data, max.data])
+    .range(["lightGreen", "green"]);
+
+var popByState = _.keyBy(populationEst2015, "state");
+
+console.log(popByState);
+
+console.log(color(popByState.AK.data))
+
+var colorDataByState = _.map(popByState, function(item){
+  return {
+    state: item.state,
+    fill: color(item.data)
+  }
+})
+
+console.log(colorDataByState);
+
+colorDataByState = _.keyBy(colorDataByState, "state")
+
+var mapData = _.mapValues(colorDataByState, function(item){
+  return item.fill
+});
+
+map.updateChoropleth(mapData);
